cbc60abd5164a49ac5c69fbe580e4d185555fa45
Kenneth Voltaire Narvaez <nkennethvoltaire@gmail.com>
2024-06-28 19:32:34 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function uploadFile() {
+    var fileInput = document.getElementById('fileInput');
+    var file = fileInput.files[0];
+    var formData = new FormData();
+    formData.append('file', file);
+
+    fetch('upload.php', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.text())
+    .then(data => {
+        alert(data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
